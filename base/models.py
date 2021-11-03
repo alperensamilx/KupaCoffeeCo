0483cdb840efe24e88014270f55f9ed08fa345d0
Alperen Samil <alperensamil05@gmail.com>
2021-11-03 15:28:35 +0300

    second commit

--- base/models.py
+++ base/models.py
@@ -9,26 +9,12 @@ class Categories(models.Model):
     # updated = models.DateTimeField(auto_now_add=True)
     # headline = models.CharField(max_length=200)
     # sub_headline = models.CharField(max_length=200, null=True, blank=True)
-    # thumbnail = models.ImageField(null=True, blank=True, upload_to="images", default="placeholder.png")
+    thumbnail = models.ImageField(null=True, blank=True, upload_to="images")
     # body = RichTextUploadingField(null=True, blank=True)
     # active = models.BooleanField(default=False)
     # featured = models.BooleanField(default=False)
     # tags = models.ManyToManyField(Tag, null=True, blank=True)
     # slug = models.SlugField(null=True, blank=True)
-
-    def __str__(self):
-        return self.name
-
-
-class Product(models.Model):
-    name = models.CharField(max_length=200)
-    description = models.TextField(null=True)
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now_add=True)
-    # price = models.DecimalField(max_digits=10, decimal_places=3)
-    # img =
-    # featured =
-    # active =
 
     def __str__(self):
         return self.name
