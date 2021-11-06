04e4fb665276f37a1f7b785d524d53ea4b3034ed
Alperen Şamil İlmaz <71771072+alperensamilx@users.noreply.github.com>
2021-11-06 21:12:57 +0300

    second commit

--- base/urls.py
+++ base/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('menu/<str:pk>/', views.menu, name='menu'),
+    path('', views.index, name='index'),
 ]
